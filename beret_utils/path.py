a70d58ab77690e683e08f53c1085d66b5a36b261
Adam Beret Manczuk <beret@hipisi.org.pl>
2021-02-01 03:08:52 +0000

    fix path function

--- beret_utils/path.py
+++ beret_utils/path.py
@@ -1,13 +1,20 @@
 import os
 
 
-def get_path():
+def get_path(i=0):
+    if i > 0:
+        def __wrap(file_name):
+            __get_path = get_path(i-1)
+            path = __get_path(file_name)
+            return os.path.dirname(path)
+        return __wrap
+
     BASE_DIR = os.getcwd()
 
-    def __path(file_name):
+    def __get_path(file_name):
         file_name = os.path.expandvars(file_name)
         if file_name[0] != '/':
             return os.path.join(BASE_DIR, file_name)
         return file_name
 
-    return __path
+    return __get_path
